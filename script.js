50c7612e12907592a75bef5650249709efb2eeba
rustimi <michele.rusticucci@gmail.com>
2024-01-25 23:38:14 +0100

    [REF] cleaner js

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-(function () {
-  const destination_date = '04/01/2024',
-    second = 1000,
-    minute = second * 60,
-    hour = minute * 60,
-    day = hour * 24;
+(function() {
+  // Constants for time units
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
 
-  const countDown = new Date(destination_date).getTime(),
-    x = setInterval(function () {
+  // Destination date
+  const destinationDate = new Date('04/01/2024').getTime();
 
-      const now = new Date().getTime(),
-        distance = countDown - now;
+  // Update countdown every second
+  const countdownInterval = setInterval(updateCountdown, 500);
 
-      document.querySelector("#days").innerText = Math.floor(distance / (day)),
-        document.querySelector("#hours").innerText = Math.floor((distance % (day)) / (hour)),
-        document.querySelector("#minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-        document.querySelector("#seconds").innerText = Math.floor((distance % (minute)) / second);
-    }, 0)
-}());
+  // Update countdown function
+  function updateCountdown() {
+    const now = new Date().getTime();
+    const distance = destinationDate - now;
+
+    // Update DOM elements
+    document.querySelector("#days").innerText = Math.floor(distance / day);
+    document.querySelector("#hours").innerText = Math.floor((distance % day) / hour);
+    document.querySelector("#minutes").innerText = Math.floor((distance % hour) / minute);
+    document.querySelector("#seconds").innerText = Math.floor((distance % minute) / second);
+
+    // Clear interval when countdown is complete
+    if (distance < 0) {
+      clearInterval(countdownInterval);
+    }
+  }
+})();
